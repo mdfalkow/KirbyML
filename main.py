7a6bf170d3980ccfd2a2cde56bfdc68679e74194
Mitchell Falkow <mdfalkow@gmail.com>
2020-04-24 21:04:13 -0400

    progress

--- main.py
+++ main.py
@@ -1,31 +1,10 @@
 import logging
-import os
-import shutil
 
 import retro
 
+from setup import setup, RECORDINGS_PATH
+
 logging.basicConfig(level=logging.INFO)
-
-SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
-ROM_PATH = f'{SCRIPT_DIR}/ROMs'
-CUSTOM_INTEGRATION_PATH = f'{SCRIPT_DIR}/custom_integrations'
-RECORDINGS_PATH = f'{SCRIPT_DIR}/recordings'
-
-def setup():
-    logging.info('Setting up ROM')
-
-    if not os.path.exists(f'{ROM_PATH}/rom.gb'):
-        raise RuntimeError('Error: rom file does not exist')
-
-    if not os.path.exists(f'{CUSTOM_INTEGRATION_PATH}/KirbysDreamLand-GameBoy/rom.gb'):
-        shutil.copy2(f'{ROM_PATH}/rom.gb',
-                     f'{CUSTOM_INTEGRATION_PATH}/KirbysDreamLand-GameBoy')
-    logging.info('Adding Kirby\'s Dream Land to Retro integrations...')
-    retro.data.Integrations.add_custom_path(CUSTOM_INTEGRATION_PATH)
-    if 'KirbysDreamLand-GameBoy' in retro.data.list_games(inttype=retro.data.Integrations.ALL):
-        logging.info('Integration successful.')
-    else:
-        raise RuntimeError('Integration failed.')
 
 
 def main():
@@ -33,10 +12,13 @@ def main():
                      inttype=retro.data.Integrations.ALL,
                      record=RECORDINGS_PATH)
     obs = env.reset()
+    i = 0
     while True:
         obs, rew, done, info = env.step(env.action_space.sample())
         env.render()
         if done:
+            logging.info(f'Iteration {i} completed.')
+            i += 1
             obs = env.reset()
     env.close()
 
